33850d25875565128e1f88b7cdd6b1be782b2d16
singy15 <uesastorage3@gmail.com>
2020-01-03 20:31:40 +0900

    Fix comments

--- src/multipart-markdown.lisp
+++ src/multipart-markdown.lisp
@@ -1,34 +1,35 @@
 (in-package :multipart-markdown)
 
+;;; Pattern for parsing
 (defvar +pattern-part+ 
   (concatenate 'string 
                "<!--[\\s]*begin-part[\\s]*(.*)[\\s]*-->"
                "([\\s\\S]*?)"
                "<!--[\\s]*end-part[\\s]*-->"))
 
+;;; Read a file as string.
 (defun slurp (path)
-  "Read string from file"
   (with-open-file (s path :direction :input)
     (let ((buf (make-string (file-length s))))
       (read-sequence buf s)
       buf)))
 
+;;; Write string to file.
+;;; Character encoding is not specified.
 (defun spit (path content)
-  "Write string to file"
   (with-open-file 
     (out path :direction :output :if-exists :supersede)
     (write-line content out)))
 
+;;; Join list of strings with separater.
 (defun join (sep str-list)
-  "Join list of strings"
   (format nil (concatenate 'string "~{~A~^" sep "~}") str-list))
 
+;;; Unpackage multipart-markdown using regular expression.
 (defun unpack (path)
-  "Unpackage multipart-markdown using regular expression"
-
   (let ((target (slurp path))
         (part (list)))
-    ;; Parse multipart markdown
+    ;; Parse multipart markdown.
     (ppcre:do-matches (s e +pattern-part+ target nil) 
       (ppcre:register-groups-bind 
         (header body) 
@@ -45,27 +46,27 @@
             (spit (cadr header) (getf m :body)))
           
           ;; type = image
+          ;; decode base64 string
           (when (equal (car header) "image")
             (ensure-directories-exist (cadr header))
             (write-base64 (cadr header) (getf m :body)))))
       part)))
 
+;;; Scan links.
 (defun scan-link (path)
-  "Scan link"
-  
   (let ((target (slurp path))
         (pattern-markdown "^[\\s]*\\[(.*)\\]\\((.*)\\)")
         (pattern-image "^[\\s]*\\!\\[(.*)\\]\\((.*)\\)")
         (links (list)))
     (mapc
       (lambda (line) 
-        ;; Scan markdown link
+        ;; Scan markdown
         (ppcre:do-matches
           (s e pattern-markdown line nil)
           (ppcre:register-groups-bind (name link) (pattern-markdown (subseq line s e)) 
             (setf links (append links (list (list :type :markdown :name name :link link))))))
         
-        ;; Scan image link
+        ;; Scan image
         (ppcre:do-matches
           (s e pattern-image line nil)
           (ppcre:register-groups-bind (name link) (pattern-image (subseq line s e)) 
@@ -73,8 +74,8 @@
       (ppcre:split #\Newline target))
     links))
 
+;;; Convert binary file to base64 string
 (defun to-base64 (path) 
-  "Convert file to base64 string"
   (let ((bytes (list)))
     (with-open-file (in path :element-type '(unsigned-byte 8))
       (loop :for b := (read-byte in nil -1)
@@ -84,6 +85,7 @@
     (with-output-to-string (out) 
       (s-base64:encode-base64-bytes (coerce bytes 'vector) out))))
 
+;;; Write binary file by base64 string
 (defun write-base64 (path base64)
   (with-open-file (out path
                      :direction :output 
@@ -93,14 +95,17 @@
           (with-input-from-string (in base64) 
             (coerce (s-base64:decode-base64-bytes in) 'list)))))
 
+;;; Convert the link for .html to a link for .md.
 (defun path-html2md (path)
   (concatenate 'string
     (namestring (cl-fad:pathname-directory-pathname (pathname path)))
     (pathname-name (pathname path))
     ".md"))
 
+;;; TODO: too complex
+;;; TODO: convert only relative link for html
+;;; Package markdown recursively.
 (defun recursive-pack (path link-type)
-  "Package markdown recursively"
   (join
     (list #\Newline)
     (list
@@ -120,12 +125,13 @@
            :initial-value "")
          ""))))
 
+;;; Package resources to a multipart-markdown.
 (defun pack (path-out path-index-md)
-  "Package resources to a multipart-markdown"
   (let ((packed (recursive-pack path-index-md :markdown)))
     (spit path-out packed)
     packed))
 
+;;; Program entry point.
 (defun main ()
   (let ((command (nth 1 sb-ext:*posix-argv*)))
     (cond ((equal command "pack") (apply #'pack (cddr sb-ext:*posix-argv*)))
